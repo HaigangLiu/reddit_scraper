913fbec7d3b2eea7fce56caa2445bead981c56f3
HaigangLiu <haigang@email.sc.edu>
2024-07-10 14:35:46 -0700

    fixed the url handling.

--- reddit_scraper.py
+++ reddit_scraper.py
@@ -4,9 +4,7 @@ import praw
 import pandas as pd
 
 
-def search_reddit_for_j1_experiences(subreddits,
-                                     keywords,
-                                     post_limit=100):
+def search_reddit_for_j1_experiences(subreddits, keywords, post_limit=100):
     """
     Search specified subreddits for given keywords related to J-1 visa experiences.
 
@@ -27,16 +25,16 @@ def search_reddit_for_j1_experiences(subreddits,
         for keyword in keywords:
             for submission in sub.search(keyword, limit=post_limit):
                 print(f"found post in r/{subreddit} for keyword '{keyword}'")
-                sleep(1)
                 results.append({
                     'search_string': keyword,
                     'subreddit': subreddit,
                     'post_title': submission.title,
                     'post_body': submission.selftext,
-                    'post_url': submission.url,
+                    'post_url': f"https://www.reddit.com{submission.permalink}",
                     'top_comment': submission.comments[0].body if submission.comments else ""
                 })
     return results
+
 
 # Define your subreddits and keywords
 subreddits = ["all"]
