18a0dfd53a1964bb1809e9eb1d5119366f8a671b
HaigangLiu <haigang@email.sc.edu>
2024-07-10 13:43:20 -0700

    update the keywords and changed the reddit to "all"

--- reddit_scraper.py
+++ reddit_scraper.py
@@ -4,7 +4,9 @@ import praw
 import pandas as pd
 
 
-def search_reddit_for_j1_experiences(subreddits, keywords, post_limit=100):
+def search_reddit_for_j1_experiences(subreddits,
+                                     keywords,
+                                     post_limit=100):
     """
     Search specified subreddits for given keywords related to J-1 visa experiences.
 
@@ -36,49 +38,22 @@ def search_reddit_for_j1_experiences(subreddits, keywords, post_limit=100):
                 })
     return results
 
-
 # Define your subreddits and keywords
-subreddits = ['immigration', 'AskAnAmerican',
-              'workandtravel', 'studyAbroad',
-              'hospitality', 'internships', 'jobs']
+subreddits = ["all"]
 keywords = [
-    '"J-1 visa" "hotel experience"',
-    '"J-1 visa" "hospitality"',
-    '"exchange visitor" "hospitality experience"',
-    '"summer work travel" "hotel"',
-    '"J-1 visa" "seasonal work" "hospitality"',
-    '"J-1 visa" "hotel internship" "US"',
-    '"J-1 visa" "resort job" "experience"',
-    '"international students" "hotel jobs"',
-    '"J-1 visa" "work and travel" "hotel"',
-    '"cultural exchange" "hotel work"',
-    '"J-1 visa stories" "hotel"',
-    '"J-1 visa experiences" "hospitality"',
-    '"J-1 visa" "working in the US" "hotels"',
-    '"J-1 visa" "customer service" "experience"',
-    '"J-1 visa" "culinary internship"',
-    '"J-1 visa" "culinary experiences"',
-    '"J-1 visa" "guest services"',
-    '"J-1 visa" "management trainee"',
-    '"J-1 visa" "business internship"',
-    '"J-1 visa traineeship" "hospitality"',
-    '"J-1 visa" "international workers"',
-    '"J-1 visa" "work exchange" "hotel"',
-    '"J-1 visa" "cultural exchange" "hospitality"',
-    '"J-1 visa" "food and beverage" "internship"',
-    '"J-1 visa" "housekeeping" "internship"',
-    '"J-1 visa" "hotel management"',
-    '"J-1 visa" "hotel operations"',
-    '"J-1 visa" "event planning"',
-    '"J-1 visa" "resort management"',
-    '"J-1 visa" "tourism industry" "experiences"'
+    "J1 hotel",
+    "J1 visa hotel",
+    "J1 visa experience",
+    "J1 visa hotel intern",
+    "J1 visa hotel summer",
+    "J1 visa hotel student",
+    "J1 visa hotel work"
 ]
-
 # Fetch results and create a DataFrame
 results = search_reddit_for_j1_experiences(subreddits, keywords)
 df = pd.DataFrame(results)
 
 # Save the DataFrame to a CSV file
-file_path = '/Users/albliu/Downloads/j1_visa_experiences.csv'
+file_path = '/Users/albliu/Downloads/j1_visa_experiences_v2.csv'
 df.to_csv(file_path, index=False)
 print(df)
